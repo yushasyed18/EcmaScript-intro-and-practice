1073fae81fd7fb35662ee3352cd6f98c841ea061
yushasyed18 <yushasyed18@gmail.com>
2025-11-28 23:25:27 +0500

    Ecmascript intro and practice

---
+++ index.js
@@ -0,0 +1,15 @@
+//alert("Do not refresh the page!")
+//consol.log("hello")
+
+//alert("Do not refresh the page!")
+//console.log("hello")
+
+//var uname = "abc"
+//console.log(typeof uname)
+
+"2" //string
+2 //number
+//console.log("hello")
+
+window.console.log('hello') // some use single quotation and some use double quotaton lekin aage jake pata lag jaye ga ke kis ko kab use krna hn or window abhi likhna zaroori nhi kyu ke abhi ham training kr rhy hn wese kuch coders window.console.log('hello') use krte hn to kuch serif console.log('hello') bhi aage jake pata lagae ke kis ko kab use krna hn......
+
